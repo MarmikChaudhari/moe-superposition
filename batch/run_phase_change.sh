772842526b60f579ecc7d4becdee3614b41f0715
Rome Thorstenson <rome.thorstenson@yale.edu>
2025-08-14 22:18:21 -1000

    phase change configs exp. 2

--- batch/run_phase_change.sh
+++ batch/run_phase_change.sh
@@ -2,9 +2,9 @@
 #SBATCH --job-name=phase_change
 #SBATCH -p gpu
 #SBATCH --gpus=1
-#SBATCH -t 18:00:00
+#SBATCH -t 23:30:00
 
 module reset
 module load miniconda
 conda activate notebook_env
-papermill phase_changes_revised.ipynb phase_changes_revised_output.ipynb
+papermill phase_changes_revised_2.ipynb phase_changes_revised_2_output.ipynb
